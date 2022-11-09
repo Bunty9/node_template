e9723dd7db699051555688d10f3503fe6f0ed740
Bunty9 <cbipin2000@gmail.com>
2022-11-09 12:36:01 +0530

    pg connection

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-const http = require(`http`)
-const express = require(`express`)
-const port = `3000`
+const http = require(`http`);
+const express = require(`express`);
+const { Client } = require("pg");
+const connectionString =
+    "postgres://pfsdzxgm:5N01-Y-tOPsBNBE5XZb1nJmwpqW7vsE2@batyr.db.elephantsql.com/pfsdzxgm";
+
+const client = new Client({
+    connectionString: connectionString,
+});
+
+client
+    .connect()
+    .then(() => {
+        console.log(`Connected to database`);
+    })
+    .catch((err) => console.log(err));
+
+const port = `3000`;
 var app = express();
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200
-    res.setHeader(`Content-Type`,`text/plain`)
-    res.end(`This is a simple nodejs application running on the port:${port}`)
-})
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader(`Content-Type`, `text/plain`);
+    res.end(`This is a simple nodejs application running on the port:${port}`);
+});
 
-app.get(`/first`, function(req, res) {
-    var result = JSON.stringify({status:200, message:"First Node JS Application"})
-    res.send(result)
-})
+app.get(`/first`, function (req, res) {
+    var result = JSON.stringify({
+        status: 200,
+        message: "First Node JS Application",
+    });
+    res.send(result);
+});
 
-app.listen(port, function(){
-    console.log(`Server is now running in the port:${port}`)
-})
+app.listen(port, function () {
+    console.log(`Server is now running in the port:${port}`);
+});
