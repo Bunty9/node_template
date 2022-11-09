357cb6a1d9b617c86efe2581fe766354c7030e4f
Bunty9 <cbipin2000@gmail.com>
2022-11-09 12:03:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require(`http`)
+const express = require(`express`)
+const port = `3000`
+var app = express();
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeader(`Content-Type`,`text/plain`)
+    res.end(`This is a simple nodejs application running on the port:${port}`)
+})
+
+app.get(`/first`, function(req, res) {
+    var result = JSON.stringify({status:200, message:"First Node JS Application"})
+    res.send(result)
+})
+
+app.listen(port, function(){
+    console.log(`Server is now running in the port:${port}`)
+})
